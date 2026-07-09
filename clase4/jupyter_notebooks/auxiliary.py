efdc8e135832fae876e0f809fe62438d1126e715
Facundo Adrián Lucianna <facundolucianna@gmail.com>
2026-07-09 17:33:45 -0300

    feat: update SVM notebook with related analysis

--- clase4/jupyter_notebooks/auxiliary.py
+++ clase4/jupyter_notebooks/auxiliary.py
@@ -69,3 +69,47 @@ def plot_boundary(  # noqa: PLR0913
     plt.ylabel(labels_axis[1])
     if legend:
         plt.legend(title=legend_title)
+
+
+def plot_svm_margins(  # noqa: PLR0913
+    x_data,
+    model,
+    max_x=(1, 1),
+    min_x=(-1, -1),
+    step_x=(0.1, 0.1),
+    colors="k",
+    alpha=1,
+    linestyles=("--", "-", "--"),
+    linewidths=(1, 3, 1),
+):
+    # Crear la malla de puntos para el gráfico
+    x1, x2 = np.meshgrid(
+        np.arange(
+            start=x_data[:, 0].min() + min_x[0],
+            stop=x_data[:, 0].max() + max_x[0],
+            step=step_x[0],
+        ),
+        np.arange(
+            start=x_data[:, 1].min() + min_x[1],
+            stop=x_data[:, 1].max() + max_x[1],
+            step=step_x[1],
+        ),
+    )
+    x_cont = np.array([x1.ravel(), x2.ravel()]).T
+
+    z = model.decision_function(x_cont)
+
+    if len(z.shape) == 1:
+        z = z.reshape(-1, 1)
+
+    for col in range(z.shape[1]):
+        plt.contour(
+            x1,
+            x2,
+            z[:, col].reshape(x1.shape),
+            colors=colors,
+            levels=[-1, 0, 1],
+            alpha=alpha,
+            linestyles=linestyles,
+            linewidths=linewidths,
+        )
